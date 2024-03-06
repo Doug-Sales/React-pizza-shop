c79bdc85b4f2d73a94e4f4267e2088df2e12688b
Doug-Sales <douglasmsales28@outlook.com>
2024-03-06 15:24:05 -0300

    MSWJS: Criação de mocks para testes

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,49 @@
+import { setupWorker } from 'msw/browser'
+
+import { env } from '@/env'
+
+import { approveOrderMock } from './approve-order-mock'
+import { cancelOrderMock } from './cancel-order-mock'
+import { deliverOrderMock } from './deliver-order-mock'
+import { dispatchOrderMock } from './dispatch-order-mock'
+import { getDailyRevenueMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-days-orders-amount'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getOrderDetailsMock } from './get-orders-details-mock'
+import { getOrdersMock } from './get-orders-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { signInMock } from './sign-in-mocks'
+import { updateProfileMock } from './update-profile-mock'
+
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthRevenueMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getDailyRevenueMock,
+  getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
+  getOrdersMock,
+  getOrderDetailsMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
+  approveOrderMock,
+)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+
+  await worker.start()
+}
